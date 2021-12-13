5c415abc2d241b26d97b936606e6843fafbc4877
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2021-12-13 22:00:08 +0530

    Create seclarge.java

---
+++ Cycle2/seclarge.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class smallar
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("How many elements: ");
+		int n = sc.nextInt();
+		int i;
+		System.out.print("Enter the elements : ");
+		int[] a = new int[50];
+		for(i=0;i<n;i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		int large;
+		int sec ;
+		if(a[0]<a[1])
+		{
+			large = a[0];
+			sec = a[1];
+		}
+		else
+		{
+			large=a[1];
+			sec = a[0];
+		}
+		for(i=0;i<n;i++)
+		{
+			if(a[i]>large)
+			{
+				sec = large;
+				large = a[i];
+			}
+			else if (a[i] > sec) 
+			{sec = a[i];}
+		}
+		System.out.println("The second largest element is " +sec);
+	}
+}
