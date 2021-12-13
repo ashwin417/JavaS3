a3db8930a00aeac3f3bc789b0f3860ac8daf644f
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2021-12-14 00:15:13 +0530

    Update strser.java

--- Cycle2/strser.java
+++ Cycle2/strser.java
@@ -1,20 +1,26 @@
 import java.util.*;
-class strser
+class Main
 {
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		String a = sc.nextLine();
 		int len = a.length();
-		int i;
+		int i,flag=0;
 		char s = sc.next().charAt(0);
 		for(i=0; i<len ;i++)
 		{
 			if(a.charAt(i)==s)
 			{
 				System.out.println("element " + s + " is present at "+ (i+1));
+				flag=-1;
+				
 				
 			}
+			
 		}
+		if(flag==0)
+		    	System.out.println("element not found");
+
 	}
 }
