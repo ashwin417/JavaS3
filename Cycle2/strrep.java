aa881f73a7db2e53004c3a4d853480174de3c38c
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2021-12-14 00:24:29 +0530

    Update strrep.java

--- Cycle2/strrep.java
+++ Cycle2/strrep.java
@@ -4,15 +4,22 @@ class strrep
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
+		
+				System.out.print("enter the string");
 		String a = sc.nextLine();
 		int len = a.length();
 		int i;
+		
+				System.out.print("enter the character to be replaced: ");
 		char s = sc.next().charAt(0);
+		
+				System.out.print("ENter the character to replace the existing character: ");
 		char r = sc.next().charAt(0);
 		
 				a = a.replace(s,r);
 				
 			
+				System.out.print("String after replacing : ");
 				System.out.println(a);
 	}
 }
