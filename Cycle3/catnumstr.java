14080d83e4d85bfd22a671c33a743741876d25e4
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-01-03 19:40:35 +0530

    Created a file

---
+++ Cycle3/catnumstr.java
@@ -0,0 +1,35 @@
+//Program to concatenate two strings and two numbers
+
+import java.util.*;
+class catnumstr
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		catnumstr ca = new catnumstr();
+		
+		System.out.println("The first string is");
+		String str1 = sc.nextLine();
+		System.out.println("The second string is");
+		String str2 = sc.nextLine();
+		System.out.println("The first number is");
+		int a = sc.nextInt();
+		System.out.println("The second number is");
+		int b = sc.nextInt();
+		ca.constrnum(str1,str2);
+		ca.constrnum(a,b);
+	}
+	void constrnum(int a, int b)
+	{
+		String p = Integer.toString(a);
+		String q = Integer.toString(b);
+		System.out.println("The concatenated number is "+ p+q);   
+	}
+	
+	void constrnum(String str1 , String str2)
+	{
+		String str3 = str1.concat(str2);
+		System.out.println("The concatenated string is "+ str3);
+	}
+	
+}
