0ca5271827bf9798894c73c3ab6e4b38c39d02bd
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-01-03 20:08:51 +0530

    Create Perimeter.java

---
+++ Cycle3/Perimeter.java
@@ -0,0 +1,29 @@
+//Program to find perimeter of a rectangle and circle using constructor overloading 
+
+import java.util.*;
+class Perimeter
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the length and breadth of Rectangle : ");
+		int l = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print("Enter the radius of circle : ");
+		int r = sc.nextInt();
+		Perimeter ab = new Perimeter(r);
+		ab = new Perimeter(l,b);
+	}
+	Perimeter(int r)
+	{
+		System.out.println("Perimeter of the circle = "+ (float)(2*3.14*r));
+	}
+	Perimeter(int l, int b)
+	{
+		System.out.println("Perimeter of the Rectangle = "+ (2*(l+b)));
+	}
+
+	
+	
+	
+}
