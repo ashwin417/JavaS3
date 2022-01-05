4f8965295e026d2e901d5f21dcbae52fe362e3d4
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-01-05 18:53:10 +0530

    Create employee.java

---
+++ Cycle3/employee.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class employe
+{
+int empno, phno;
+String name;
+}
+class employee
+{
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number of employee : ");
+    int n = sc.nextInt();
+    employe e[] = new employe[n];
+    for(int i=0; i<n ; i++)
+    {
+    e[i] = new employe();
+        System.out.print("Enter the employee number of Employee " + (i+1)+":");
+        e[i].empno = sc.nextInt();
+        System.out.print("Enter the name of employee ");
+        e[i].name = sc.next();
+        System.out.print("Enter the Phone Number of employee ");
+        e[i].phno = sc.nextInt();
+    }
+    for(int j=0;j<n;j++)
+    {
+    System.out.println();
+    System.out.println("Employee ID : " + e[j].empno);
+    System.out.println("Employee name : "+e[j].name);
+    System.out.println("Employee Phone Number : " + e[j].phno);
+    }
+}
+}
