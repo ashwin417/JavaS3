e706c75441ef93dfeaa72563b6b3eba0f87b0291
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-01-23 09:51:50 +0530

    Update Perimeter.java

--- Cycle3/Perimeter.java
+++ Cycle3/Perimeter.java
@@ -6,21 +6,21 @@ class Perimeter
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the length and breadth of Rectangle : ");
+		System.out.print("Enter the length and breadth of Rectangle in cm: ");
 		int l = sc.nextInt();
 		int b = sc.nextInt();
-		System.out.print("Enter the radius of circle : ");
+		System.out.print("Enter the radius of circle in cm : ");
 		int r = sc.nextInt();
 		Perimeter ab = new Perimeter(l,b);
 		ab = new Perimeter(r);
 	}
 	Perimeter(int r)
 	{
-		System.out.println("Perimeter of the circle = "+ (float)(2*3.14*r)+" sq.cm");
+		System.out.println("Perimeter of the circle = "+ (float)(2*3.14*r)+" cm");
 	}
 	Perimeter(int l, int b)
 	{
-		System.out.println("Perimeter of the Rectangle = "+ (2*(l+b))+" sq.cm");
+		System.out.println("Perimeter of the Rectangle = "+ (2*(l+b))+" cm");
 	}
 
 	
