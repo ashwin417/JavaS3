4c61880b4945591e8cb892a1ec75fe3b1cea4031
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-02-07 08:07:36 +0530

    Add files via upload

---
+++ Cycle6/experiment2.java
@@ -0,0 +1,26 @@
+
+class experiment2
+{
+	public static void main(String[] args )
+	{
+		try{
+			int b = 100;
+			if(b<500)
+				throw new balanceException();
+		
+		}
+		catch(balanceException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
+
+/*class balanceException extends Exception
+{
+	balanceException()
+	{
+		System.out.print("Low balance ");
+	}
+}
+*/
